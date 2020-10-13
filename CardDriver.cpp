5627ed0f59d01189edb64f824a6d23d8251fff0e
Claudia <claudia@bliinx.ca>
2020-10-12 21:51:09 -0400

    Cards

--- CardDriver.cpp
+++ CardDriver.cpp
@@ -8,57 +8,53 @@
 #include <iostream>
 #include <vector>
 using namespace std;
-
-
 int main(){
+cout << "Welcome to the world's greatest card displayer! \n\n";
 
-    cout << "Welcome to the world's greatest card displayer! \n\n";
+Hand *hand = new Hand();
+Deck *deck = new Deck();
 
-    Hand hand;
-    Deck deck;
-    for(int i = 1; i > 0;){
-        cout << "What do you want to do? \n 1. Play a card \n 2. Draw a card \n 3. Display your hand \n 4. Quit \n";
-        int menu = 0;
-        cin >> menu;
+int menu = 0;
 
-        if(menu == 1){
+while(menu != 4) {
+    cout << "What do you want to do? \n 1. Play a card \n 2. Draw a card \n 3. Display your hand \n 4. Quit \n";
+    cin >> menu;
 
-        cout << "What card do you want to play? \n1. Bomb \n2. Blockade\n3. Airlift \n4. Reinforcements \n5. Diplomacy\n\n";
-        int option;
-        cin >> option;
+    if(menu == 1){
 
-        if(option == 1)
-            deck.add(hand.play("bomb"));
-        else if(option == 2)
-            deck.add(hand.play("blockade"));
-        else if(option == 3)
-            deck.add(hand.play("airlift"));
-        else if(option == 4)
-        deck.add(hand.play("reinforcements"));
-        else if(option == 5)
-            deck.add(hand.play("diplomacy"));
-        else
-            cout << "Error: Please enter a valid menu option.\n\n";
-        }
+    cout << "What card do you want to play? \n1. Bomb \n2. Blockade\n3. Airlift \n4. Reinforcements \n5. Diplomacy\n\n";
+    int option;
+    cin >> option;
 
-        else if(menu == 2){
+    if(option == 1)
+        deck->add(hand->play("bomb"));
+    else if(option == 2)
+        deck->add(hand->play("blockade"));
+    else if(option == 3)
+        deck->add(hand->play("airlift"));
+    else if(option == 4)
+    deck->add(hand->play("reinforcements"));
+    else if(option == 5)
+        deck->add(hand->play("diplomacy"));
+    else
+        cout << "Error: Please enter a valid menu option.\n\n";
+    }
 
-            hand.add(deck.draw());
-        }
+    else if(menu == 2){
+        
+        hand->add(deck->draw());
+    }
 
-        else if(menu == 3){
-            hand.display();
-        }
+    else if(menu == 3){
+        hand->display();
+    }
 
-        else if(menu == 4){
-            cout << "Thanks for playing!";
-            return 0;
-        }
+    else{
+        cout << "Error: Please select a valid menu option. \n";
+    }
 
-        else{
-            cout << "Error: Please select a valid menu option. \n";
-        }
-        }
+    cout << "Thanks for playing!";
+}
 
-    return 0;
+return 0;
 }
--- Cards.cpp
+++ Cards.cpp
@@ -3,8 +3,12 @@
  COMP 345 - D
  9 October 2020
  */
-
+#include <stdio.h>
+#include <stdlib.h>
 #include "Cards.h"
+#include <iostream>
+#include <vector>
+#include <exception>
 
 using namespace std;
 
@@ -36,8 +40,9 @@ Card::Card(const Card &oldCard) {
     type = oldCard.type;
 }
 
-ostream &cardStream(ostream &cs, const Card c) {
-    cout << "Card type: " << c.type;
+
+ostream &cardStream(ostream &cs, const Card *c) {
+    cout << "Card type: " << c->type;
 }
 
 string Card::getType() {
@@ -52,108 +57,12 @@ void Card::setType(string x) {
  * Initializes 50 pseudo-random cards and sorts them into a vector
  * */
 Deck::Deck() {
-    Card Card1;
-    Card Card2;
-    Card Card3;
-    Card Card4;
-    Card Card5;
-    Card Card6;
-    Card Card7;
-    Card Card8;
-    Card Card9;
-    Card Card10;
-    deck.push_back(Card1);
-    deck.push_back(Card2);
-    deck.push_back(Card3);
-    deck.push_back(Card4);
-    deck.push_back(Card5);
-    deck.push_back(Card6);
-    deck.push_back(Card7);
-    deck.push_back(Card8);
-    deck.push_back(Card9);
-    deck.push_back(Card10);
-    Card Card11;
-    Card Card12;
-    Card Card13;
-    Card Card14;
-    Card Card15;
-    Card Card16;
-    Card Card17;
-    Card Card18;
-    Card Card19;
-    Card Card20;
-    deck.push_back(Card11);
-    deck.push_back(Card12);
-    deck.push_back(Card13);
-    deck.push_back(Card14);
-    deck.push_back(Card15);
-    deck.push_back(Card16);
-    deck.push_back(Card17);
-    deck.push_back(Card18);
-    deck.push_back(Card19);
-    deck.push_back(Card20);
-    Card Card21;
-    Card Card22;
-    Card Card23;
-    Card Card24;
-    Card Card25;
-    Card Card26;
-    Card Card27;
-    Card Card28;
-    Card Card29;
-    Card Card30;
-    deck.push_back(Card21);
-    deck.push_back(Card22);
-    deck.push_back(Card23);
-    deck.push_back(Card24);
-    deck.push_back(Card25);
-    deck.push_back(Card26);
-    deck.push_back(Card27);
-    deck.push_back(Card28);
-    deck.push_back(Card29);
-    deck.push_back(Card30);
-    Card Card31;
-    Card Card32;
-    Card Card33;
-    Card Card34;
-    Card Card35;
-    Card Card36;
-    Card Card37;
-    Card Card38;
-    Card Card39;
-    Card Card40;
-    deck.push_back(Card31);
-    deck.push_back(Card32);
-    deck.push_back(Card33);
-    deck.push_back(Card34);
-    deck.push_back(Card35);
-    deck.push_back(Card36);
-    deck.push_back(Card37);
-    deck.push_back(Card38);
-    deck.push_back(Card39);
-    deck.push_back(Card40);
-    Card Card41;
-    Card Card42;
-    Card Card43;
-    Card Card44;
-    Card Card45;
-    Card Card46;
-    Card Card47;
-    Card Card48;
-    Card Card49;
-    Card Card50;
-    deck.push_back(Card41);
-    deck.push_back(Card42);
-    deck.push_back(Card43);
-    deck.push_back(Card44);
-    deck.push_back(Card45);
-    deck.push_back(Card46);
-    deck.push_back(Card47);
-    deck.push_back(Card48);
-    deck.push_back(Card49);
-    deck.push_back(Card50);
-}
+    Card *temp = new Card;
+    for (int i = 0; i < 50; i++) {
+        deck.push_back(temp);
+    }
 
+}
 
 Deck::Deck(const Deck &oldDeck) {
     for (int i = 0; i < oldDeck.deck.size(); i++) {
@@ -161,23 +70,34 @@ Deck::Deck(const Deck &oldDeck) {
     }
 }
 
-ostream &deckStream(ostream &ds, const Deck d) {
-    cout << "Cards left in the deck: \n";
-    for (int i = 0; i < d.deck.size(); i++) {
-        Card temp = d.deck.at(i);
-        cout << temp.getType() << "\n";
+Deck::~Deck() {
+    for (int i = 0; i < deck.size(); i++) {
+        delete deck[i];
+        deck[i] = NULL;
     }
+
 }
 
-vector <Card> Deck::getDeck() {
+ostream &deckStream(ostream &ds, const Deck *d) {
+    Card *temp = new Card;
+    cout << "Cards left in the deck: \n";
+    for (int i = 0; i < d->deck.size(); i++) {
+        temp = d->deck.at(i);
+        cout << temp->getType() << "\n";
+    }
+    delete temp;
+    temp = NULL;
+}
+
+vector<Card*> Deck::getDeck() {
     return deck;
 }
 
-void Deck::setDeck(vector <Card> cv) {
+void Deck::setDeck(vector<Card*> cv) {
     deck = cv;
 }
 
-void Deck::add(Card card) {
+void Deck::add(Card *card) {
     deck.insert(deck.begin(), card);
 }
 
@@ -185,31 +105,27 @@ void Deck::add(Card card) {
  * Draws a card, puts it in the hand and erases it from the deck 
  */
 
-Card Deck::draw() {
-    Card oldCard;
+Card *Deck::draw() {
+    Card *oldCard = new Card;
     if (deck.size() == 0) {
         return oldCard;
+        delete oldCard;
     }
-    cout << "You got " << deck.back().getType() << "!\n\n";
+    cout << "You got " << deck.back()->getType() << "!\n\n";
     oldCard = deck.back();
     deck.pop_back();
     return oldCard;
+    delete oldCard;
 }
 
 Hand::Hand() {
     /**
      * Initializes a hand with 5 cards
      */
-    Card Hand1;
-    Card Hand2;
-    Card Hand3;
-    Card Hand4;
-    Card Hand5;
-    hand.push_back(Hand1);
-    hand.push_back(Hand2);
-    hand.push_back(Hand3);
-    hand.push_back(Hand4);
-    hand.push_back(Hand5);
+    Card *temp = new Card;
+    for (int i = 0; i < 5; i++) {
+        hand.push_back(temp);
+    }
     bombCounter = 0;
     reinforcementCounter = 0;
     blockadeCounter = 0;
@@ -228,38 +144,49 @@ Hand::Hand(const Hand &oldHand) {
     diplomacyCounter = oldHand.diplomacyCounter;
 }
 
-ostream &handStream(ostream &hs, const Hand h) {
+ostream &handStream(ostream &hs, const Hand *h) {
+    Card *temp = new Card;
     cout << "Cards in hand: \n";
-    for (int i = 0; i < h.hand.size(); i++) {
-        Card temp = h.hand.at(i);
-        cout << temp.getType() << "\n";
+    for (int i = 0; i < h->hand.size(); i++) {
+        temp = h->hand.at(i);
+        cout << temp->getType() << "\n";
 
+    }
+}
+
+Hand::~Hand() {
+    for(int i = 0; i < hand.size();i++){
+        delete hand[i];
+        hand[i] = NULL;
     }
 }
 
 /**
  * Plays a card, returns it to the deck and erases it from the hand
  */
-Card Hand::play(string cardType) {
-    Card oldCard;
+Card *Hand::play(string cardType) {
+    Card *oldCard = new Card;
     for (int i = 0; i < hand.size(); i++) {
-        if (hand.at(i).getType() == cardType) {
+        if (hand.at(i)->getType() == cardType) {
             oldCard = hand.at(i);
             cout << "You played " << cardType << "! \n\n";
             hand.erase(hand.begin() + i);
             return oldCard;
         }
     }
+
+    cout << "You don't have any " + cardType + "s in your hand!\n\n";
+    return oldCard;
 }
 
 /**
  * Adds a new card to the hand from the deck (Complementary to "draw" function)
  * */
-void Hand::add(Card card) {
+void Hand::add(Card *card) {
     try {
         hand.push_back(card);
 
-        if (card.getType() == "") {
+        if (card->getType() == "") {
             throw 99;
         }
     }
@@ -272,16 +199,17 @@ void Hand::add(Card card) {
  * Displays the number of each card in the hand after counting them
  */
 void Hand::display() {
+
     for (int i = 0; i < hand.size(); i++) {
-        if (hand.at(i).getType() == "bomb")
+        if (hand.at(i)->getType() == "bomb")
             bombCounter += 1;
-        else if (hand.at(i).getType() == "reinforcements")
+        else if (hand.at(i)->getType() == "reinforcements")
             reinforcementCounter += 1;
-        else if (hand.at(i).getType() == "blockade")
+        else if (hand.at(i)->getType() == "blockade")
             blockadeCounter += 1;
-        else if (hand.at(i).getType() == "airlift")
+        else if (hand.at(i)->getType() == "airlift")
             airliftCounter += 1;
-        else if (hand.at(i).getType() == "diplomacy")
+        else if (hand.at(i)->getType() == "diplomacy")
             diplomacyCounter += 1;
     }
     cout << "Here are the cards in your hand:";
@@ -337,11 +265,11 @@ void Hand::setDipCount(int x) {
     diplomacyCounter = x;
 }
 
-vector <Card> Hand::getHand() {
+vector<Card*> Hand::getHand() {
     return hand;
 }
 
-void Hand::setHand(vector <Card> cv) {
+void Hand::setHand(vector<Card*> cv) {
     hand = cv;
 }
 
