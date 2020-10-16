1eb82cb57142a41968c34b936679ba8c50367c00
ginibrandon <gini@forsaw.ca>
2020-10-16 14:18:40 -0400

    Latest commits

--- PlayerDriver.cpp
+++ PlayerDriver.cpp
@@ -8,16 +8,19 @@
 using namespace std;
 
 int main() {
+	cout << "Creating new player...";
 	vector<Territory*> playerTerritories = { new Territory("1 England 1 164 126", "1 8 21 6 7 5 2 3 4") };
 	Hand* playerHand = new Hand();
 	OrderList* playerOrders = new OrderList();
 	Player* testPlayer = new Player(playerTerritories, playerHand, playerOrders);
-	cout << "Player info:" << *testPlayer;
+	cout << "\nPlayer info:" << *testPlayer;
 	
-	//Player* copyPlayer = testPlayer;
+	cout << "\nCopying player...";
+	Player* copyPlayer = testPlayer;
+	cout << "\nCopied player info: " << *copyPlayer;
 
-	//vector<Territory*> toDefend = testPlayer->toDefend();
-	//vector<Territory*> toAttack = testPlayer->toAttack();
-	//testPlayer->issueOrder();
+	vector<Territory*> toDefend = testPlayer->toDefend();
+	vector<Territory*> toAttack = testPlayer->toAttack();
+	testPlayer->issueOrder();
 
 }
